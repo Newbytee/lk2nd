d591e5a1fca8a0333782eaa8ce2eb623be4370cf
Minecrell <minecrell@minecrell.net>
2021-06-10 17:57:20 +0200

    tmp

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -1,35 +1,4 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 DTBS += \
-	$(LOCAL_DIR)/apq8016-samsung-r02.dtb \
-	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
-	$(LOCAL_DIR)/apq8016-sbc.dtb \
-	$(LOCAL_DIR)/msm8216-samsung-r05.dtb \
-	$(LOCAL_DIR)/msm8916-asus-z00l.dtb \
-	$(LOCAL_DIR)/msm8916-huawei-g7-l01.dtb \
-	$(LOCAL_DIR)/msm8916-huawei-hwt1a21l.dtb \
-	$(LOCAL_DIR)/msm8916-lg.dtb \
-	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4d.dtb \
-	$(LOCAL_DIR)/msm8916-motorola-harpia-p1b-4e.dtb \
-	$(LOCAL_DIR)/msm8916-motorola-osprey.dtb \
-	$(LOCAL_DIR)/msm8916-motorola-surnia.dtb \
-	$(LOCAL_DIR)/msm8916-mtp.dtb \
-	$(LOCAL_DIR)/msm8916-mtp-smb1360.dtb \
-	$(LOCAL_DIR)/msm8916-qrd5-v1.dtb \
-	$(LOCAL_DIR)/msm8916-qrd7+12-v1.dtb \
-	$(LOCAL_DIR)/msm8916-qrd8-v1.dtb \
-	$(LOCAL_DIR)/msm8916-qrd9-v1.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r00.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r02.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r04.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r05.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r06.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r08.dtb \
-	$(LOCAL_DIR)/msm8929-samsung-r00.dtb \
-	$(LOCAL_DIR)/msm8929-samsung-r04.dtb \
-	$(LOCAL_DIR)/msm8939-asus-z00t.dtb \
-	$(LOCAL_DIR)/msm8939-huawei-kiwi.dtb \
-	$(LOCAL_DIR)/msm8939-mtp.dtb \
-	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb
+	$(LOCAL_DIR)/tmp.dtb
